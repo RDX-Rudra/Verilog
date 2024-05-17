a610d56ff465e53697f3f279c539ec30cca5b6fa
RDX-Rudra <dasrudra234@gmail.com>
2024-05-17 08:45:49 +0530

    verilog

--- Adder/rippleCarryAdder.v
+++ Adder/rippleCarryAdder.v
@@ -8,3 +8,15 @@ module full_adder1b(
     assign cout = ((a & b) | (b & c) | (c & a));
 endmodule
 
+module parallel_adder4b (
+    input [3:0]x, y;
+    input cin,
+    output [3:0] sum,
+    output carry
+);
+    wire cout1, cout2, cout3;
+    full_adder1b FA1 (x[0], y[0], cin, sum[0], cout1);
+    full_adder1b FA1 (x[1], y[1], cout1, sum[1], cout2);
+    full_adder1b FA1 (x[2], y[2], cout2, sum[2], cout3);
+    full_adder1b FA1 (x[3], y[3], cout3, sum[3], carry);
+endmodule
