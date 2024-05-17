b7a025954d883464d5eadcd2f71c9df973955e75
RDX-Rudra <dasrudra234@gmail.com>
2024-05-17 09:13:09 +0530

    vlog done

--- Adder/adderCumSubtractor.v
+++ Adder/adderCumSubtractor.v
@@ -7,33 +7,22 @@ module full_adder1b(
 
 endmodule
 
-module inverse(
-    input [3:0] bin2,
-    input cin,
-    output [3:0] bin3
-);  
-
-    wire [3:0] select = {cin,cin,cin,cin};
-    assign bin3 = (select ^ bin2);
-
-endmodule
-
-
-module adder4b(
-    input [3:0] bin1, 
-    input [3:0] bin3,
+module adder_cum_subtractor4b (
+    input [3:0]x, z,
     input cin,
     output [3:0] sum,
     output carry
-);  
-    
+);
     wire cout1, cout2, cout3;
-
-    full_adder1b adder1(bin1[0], bin3[0], cin, sum[0], cout1);
-    full_adder1b adder2(bin1[1], bin3[1], cout1, sum[1], cout2);
-    full_adder1b adder3(bin1[2], bin3[2], cout2, sum[2], cout3);
-    full_adder1b adder4(bin1[3], bin3[3], cout3, sum[3], carry);
-     
+    wire [3:0]y;
+    xor(y[0], z[0], cin);
+    xor(y[1], z[1], cin);
+    xor(y[2], z[2], cin);
+    xor(y[3], z[3], cin);
+    full_adder1b FA1 (x[0], y[0], cin, sum[0], cout1);
+    full_adder1b FA2 (x[1], y[1], cout1, sum[1], cout2);
+    full_adder1b FA3 (x[2], y[2], cout2, sum[2], cout3);
+    full_adder1b FA4 (x[3], y[3], cout3, sum[3], carry);
 endmodule
 
 module tb_adder4b(
@@ -44,7 +33,7 @@ module tb_adder4b(
 );
 	initial begin
 		$monitor("%t, When bin1=%b, bin2=%b cin=%b then Result is =%b , Carry =%b",$time,bin1,bin2,cin,sum,carry);
-		   {bin1,bin2,cin} = 9'b010110100;
+		   {bin1,bin2,cin} = 9'b010110101;
 		#5 {bin1,bin2,cin} = 9'b001111101;
 		#5 {bin1,bin2,cin} = 9'b111100110;
 		#5 {bin1,bin2,cin} = 9'b011100011;
@@ -60,10 +49,9 @@ module wb;
     wire [3:0] bin1, bin2, sum, bin3;
     wire cin, carry;
     initial begin
-        $dumpfile("adder.vcd");
+        $dumpfile("addersubtractor.vcd");
         $dumpvars(0, wb);
     end
-    inverse newbin (bin2,cin,bin3);
-    adder4b dut (bin1, bin3, cin, sum, carry);
+    adder_cum_subtractor4b dut (bin1, bin2, cin, sum, carry);
     tb_adder4b tb (bin1, bin2, cin, sum, carry);
 endmodule
