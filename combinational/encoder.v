279d6d730a4768cc020669a6a1f6787b49eb2805
RDX-Rudra <dasrudra234@gmail.com>
2024-09-08 01:36:57 +0530

    verilog

--- combinational/encoder.v
+++ combinational/encoder.v
@@ -6,7 +6,18 @@ module encoder_1hot_4to2 (
     assign YZ[0] = ABCD[3] | ABCD[1];
 endmodule
 
-
+module tb_encoder (
+    output reg [3:0] ABCD,
+    input [1:0] YZ
+);
+    initial begin
+        ABCD = 4'b0000;  
+        #5 ABCD = 4'b0001;
+        #5 ABCD = 4'b0010;
+        #5 ABCD = 4'b0100;
+        #5 ABCD = 4'b1000;
+    end
+endmodule
 
 module wb_en;
     wire [3:0] ABCD;
